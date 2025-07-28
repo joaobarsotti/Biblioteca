6e28a5036d4f722f8689dc0e9e7cb95ac58153a6
Joao Pedro <joao.barsotti@hotmail.com>
2025-07-28 11:08:48 -0300

    Primeiro commit - estrutura inicial do projeto

---
+++ Livro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca {
+    public class Livro {
+        //public int Codigo { get; set; }
+       // public string Autor { get; set; }
+        public string Titulo { get; set; }
+        //public int Paginas { get; set; }
+    }
+}
---
+++ ProgramaPrincipal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca {
+    class Program {
+        static List<Livro> livros = new List<Livro>();
+        static void Main(string[] args) {
+            int opcao;
+
+            do {
+                Console.WriteLine("___ MENU PRINCIPAL ___");
+                Console.WriteLine("1- Cadastrar");
+                Console.WriteLine("2- Alterar");
+                Console.WriteLine("3- Exibir");
+                Console.WriteLine("4- Excluir");
+                Console.WriteLine("5- Sair");
+                Console.Write("Digite sua opção: ");
+                opcao = int.Parse(Console.ReadLine()!);
+
+                switch (opcao) {
+                    case 1:
+                        CadastrarLivro();
+                        break;
+                    case 2:
+                        Console.WriteLine("ALTERAR");
+                        break;
+                    case 3:
+                        ExibirLivros();
+                        break;
+                    case 4:
+                        Console.WriteLine("EXCLUIR");
+                        break;
+                    case 5:
+                        Console.WriteLine("Saindo do programa");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            } while (opcao != 5);
+        }
+
+        static void CadastrarLivro() {
+            Console.Clear();
+            Console.WriteLine("CADASTRAR LIVRO\n");
+
+            /*Console.WriteLine("Código: ");
+            int codigo = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Autor: ");
+            string autor = Console.ReadLine()!;
+            */
+            Console.WriteLine("Título: ");
+            string titulo = Console.ReadLine()!;
+
+           // Console.WriteLine("Número de páginas: ");
+           // int paginas = int.Parse(Console.ReadLine()!);
+
+            Livro novoLivro = new Livro() {
+               // Codigo = codigo,
+               // Autor = autor,
+                Titulo = titulo
+                // Paginas = paginas
+            };
+
+            livros.Add(novoLivro);
+            Console.WriteLine("Livro cadastrado com sucesso!");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+
+        static void ExibirLivros() {
+            Console.Clear();
+            Console.WriteLine("EXIBIR LIVROS\n");
+            //for(int i = 0; i < livros.Count; i++) {
+              //  Console.WriteLine($"Título: {livros[i].Titulo}");
+            //}
+            foreach (Livro livro in livros) {
+                Console.WriteLine($"Título: {livro.Titulo}");
+            }
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
