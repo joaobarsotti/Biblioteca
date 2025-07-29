6f25805b0046a72bd89bcab1e0486a1beccf1a95
Joao Pedro <joao.barsotti@hotmail.com>
2025-07-29 10:46:37 -0300

    Programando a opção 3 Alterar Livro e criando uma função para o Menu Principal

--- ProgramaPrincipal.cs
+++ ProgramaPrincipal.cs
@@ -8,9 +8,9 @@ namespace Biblioteca {
     class Program {
         static List<Livro> livros = new List<Livro>();
         static void Main(string[] args) {
-            int opcao;
-
-            do {
+            ExibirOpcoesDoMenu();
+        }
+            static void ExibirOpcoesDoMenu() {
                 Console.WriteLine("___ MENU PRINCIPAL ___");
                 Console.WriteLine("1- Cadastrar");
                 Console.WriteLine("2- Alterar");
@@ -18,14 +18,14 @@ namespace Biblioteca {
                 Console.WriteLine("4- Excluir");
                 Console.WriteLine("5- Sair");
                 Console.Write("Digite sua opção: ");
-                opcao = int.Parse(Console.ReadLine()!);
+                int opcao = int.Parse(Console.ReadLine()!);
 
                 switch (opcao) {
                     case 1:
                         CadastrarLivro();
                         break;
                     case 2:
-                        Console.WriteLine("ALTERAR");
+                        AlterarLivro();
                         break;
                     case 3:
                         ExibirLivros();
@@ -40,50 +40,71 @@ namespace Biblioteca {
                         Console.WriteLine("Opção inválida!");
                         break;
                 }
-            } while (opcao != 5);
-        }
-
-        static void CadastrarLivro() {
-            Console.Clear();
-            Console.WriteLine("CADASTRAR LIVRO\n");
-
-            /*Console.WriteLine("Código: ");
-            int codigo = int.Parse(Console.ReadLine()!);
-
-            Console.WriteLine("Autor: ");
-            string autor = Console.ReadLine()!;
-            */
-            Console.WriteLine("Título: ");
-            string titulo = Console.ReadLine()!;
-
-           // Console.WriteLine("Número de páginas: ");
-           // int paginas = int.Parse(Console.ReadLine()!);
-
-            Livro novoLivro = new Livro() {
-               // Codigo = codigo,
-               // Autor = autor,
-                Titulo = titulo
-                // Paginas = paginas
-            };
-
-            livros.Add(novoLivro);
-            Console.WriteLine("Livro cadastrado com sucesso!");
-            Thread.Sleep(1000);
-            Console.Clear();
-        }
-
-        static void ExibirLivros() {
-            Console.Clear();
-            Console.WriteLine("EXIBIR LIVROS\n");
-            //for(int i = 0; i < livros.Count; i++) {
-              //  Console.WriteLine($"Título: {livros[i].Titulo}");
-            //}
-            foreach (Livro livro in livros) {
-                Console.WriteLine($"Título: {livro.Titulo}");
             }
-            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-        }
+
+            static void CadastrarLivro() {
+                Console.Clear();
+                Console.WriteLine("CADASTRAR LIVRO\n");
+                /*
+                Console.WriteLine("Código: ");
+                int codigo = int.Parse(Console.ReadLine()!);
+                
+                Console.WriteLine("Autor: ");
+                string autor = Console.ReadLine()!;
+                */
+                Console.Write("Título: ");
+                string titulo = Console.ReadLine()!;
+
+                // Console.WriteLine("Número de páginas: ");
+                // int paginas = int.Parse(Console.ReadLine()!);
+
+                Livro novoLivro = new Livro() {
+                    //Codigo = codigo,
+                    // Autor = autor,
+                    Titulo = titulo
+                    // Paginas = paginas
+                };
+
+                livros.Add(novoLivro);
+                Console.WriteLine("Livro cadastrado com sucesso!");
+                Thread.Sleep(1000);
+                Console.Clear();
+                ExibirOpcoesDoMenu();
+            }
+
+            static void AlterarLivro() {
+                Console.Clear();
+                Console.WriteLine("ALTERAR LIVRO \n");
+                Console.Write("Digite o código do livro que deseja alterar: ");
+                string titulo = Console.ReadLine()!;
+
+                Livro livroParaAlterar = livros.Find(l => l.Titulo == titulo)!;
+
+                if (livroParaAlterar != null) {
+                    Console.WriteLine("Livro encontrado!");
+                    Console.Write("Digite o novo título:");
+                    string novoTitulo = Console.ReadLine()!;
+                    livroParaAlterar.Titulo = novoTitulo;
+                    Console.WriteLine("Livro alterado com sucesso!");
+                } else {
+                    Console.WriteLine($"O livro {titulo} não foi encontrado!");
+                }
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+                ExibirOpcoesDoMenu();
+            }
+
+            static void ExibirLivros() {
+                Console.Clear();
+                Console.WriteLine("EXIBIR LIVROS\n");
+                foreach (Livro livro in livros) {
+                    Console.WriteLine($"Título: {livro.Titulo}");
+                }
+                Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+                ExibirOpcoesDoMenu();
+            }
+        }    
     }
-}
